001ad58fd0f9bf6e9fd0626830f7f392bd847e94
Ivan Vedernikov <ecugol@gmail.com>
2021-03-09 05:13:34 +0500

    bump version

--- jet/__init__.py
+++ jet/__init__.py
@@ -2,7 +2,7 @@
 Next Generation of django-jet (Modern template for Django admin interface with improved functionality)
 """
 
-__version__ = '1.0.12'
+__version__ = '1.0.13'
 __author__ = 'TIK (Technology Innovation Network)'
 __license__ = 'AGPLv3'
 __copyright__ = u'2015-2018, Geex Arts. 2019-2020, Technology Innovation Network'
